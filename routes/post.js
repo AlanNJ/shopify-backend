759bd10db84d5c5a46f45cf6e37ba967f838a662
AlanNJ <alannpn75@gmail.com>
2022-04-08 23:15:19 +0530

    added-new-route

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,13 @@
 const express = require("express");
+
 const getAllPosts = require("../controllers/getAllPosts");
+const getSinglePost = require("../controllers/getSinglePost");
 const uploadPost = require("../controllers/uploadPost");
 
 const router = express.Router();
 router.get("/get-post", getAllPosts);
 router.post("/upload-post", uploadPost);
+router.get("/get-single-post/:_id", getSinglePost);
+router.get("/get-cart-products", getCartProducts);
 
 module.exports = router;
