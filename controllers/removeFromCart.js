cf4947b6fe445bd1bddfed7e99cbfe504437f6cf
AlanNJ <alannpn75@gmail.com>
2022-04-13 21:03:06 +0530

    third-commit

--- controllers/removeFromCart.js
+++ controllers/removeFromCart.js
@@ -1,28 +1,33 @@
 const User = require("../models/user");
 const removeFromCart = async (req, res) => {
 	const id = req.params._id;
-	console.log(typeof id);
 
 	//console.log(req.headers.id);
-	const user = await User.findOne({ _id: req.headers.id });
-	//console.log(user.newCart);
+	const user = await User.findOne({ _id: req.body.headers.id });
+	//console.log(user);
 
-	// user.newCart.forEach((item) => {
-	// 	if (item.post._id == id._id) {
-	// 		const itemToDelete = item;
-	// 	}
-	// });
+	//  user.newCart.forEach((item) => {
+	//  	if (item.post._id == id) {
+	//  		user.newCart.pop();
+	//  	}
+	//  });
 
 	User.updateOne(
 		{ _id: "6252d87e010b3f94adcf4e41" },
 		{
 			$pull: {
-				"newCart.post": {
-					_id: "6249e216c1e3b6ae2d4aa273",
+				newCart: {
+					"post.id": "6249e216c1e3b6ae2d4aa273",
 				},
 			},
 		},
 		{ new: true }
-	).then((res) => console.log(res, user));
+	)
+		.exec()
+		.then((res) =>
+			User.findOne({ _id: "6252d87e010b3f94adcf4e41" }).then((res) =>
+				console.log(res)
+			)
+		);
 };
 module.exports = removeFromCart;
